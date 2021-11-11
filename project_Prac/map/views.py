b731715863eb2ca5f5a8238031b5805f22c99028
jay <jaewhan232@naver.com>
2021-11-11 16:04:51 +0900

    도로명주소 좌표 변환 객체 생성 및 지도 표시

--- project_Prac/map/views.py
+++ project_Prac/map/views.py
@@ -3,4 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def showmap(request):
-    return render(request,'map_page.html')
+    home_address="경기도 화성시 동탄대로 6길 20"
+    return render(request,'map_page.html',{'home_address':home_address})
