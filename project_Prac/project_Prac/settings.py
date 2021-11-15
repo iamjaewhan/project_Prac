cd7ce629b52ae70180459d77b05d38f47d1ac5a9
jay <jaewhan232@naver.com>
2021-11-15 15:36:26 +0900

    MySQL DB 연결

--- project_Prac/project_Prac/settings.py
+++ project_Prac/project_Prac/settings.py
@@ -87,8 +87,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Project_Prac',
+        'USER' : 'jay',
+        'PASSWORD':'mWodhks22#',
+        'HOST':'localhost',
+        'PORT':'3306'
     }
 }
 
